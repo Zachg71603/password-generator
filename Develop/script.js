d6fe685470013ddd77efe7164b944aab8752fe78
Zach Gutierrez <zg71603@gmail.com>
2023-11-06 21:53:55 -0800

    Adding in if statement if 1/4 options are selected

--- Develop/script.js
+++ Develop/script.js
@@ -115,4 +115,29 @@ else if (acceptNumericValue === true & acceptUppercase === true) {
   
 } 
 
+//if user selects 1/4 character types
+
+else if (acceptNumericValue === true) {
+  selected = number;
+  
+} 
+
+else if (acceptSpecialChar === true) {
+  selected = specialChar;
+  
+} 
+
+else if (acceptUppercase === true) {
+  selected = upperCaseLtr;
+  
+} 
+
+else if (acceptLowercase === true) {
+  selected = lowerCaseLtr;
+  
+}
+
+
+
+
 
